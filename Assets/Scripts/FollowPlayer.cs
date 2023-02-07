64f2be31c014483130fecef3904326762d2ec37b
yonatanin <yoyocnolasco@s.sfusd.edu>
2023-02-07 09:58:32 -0800

    make the car go forward and left and right

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
 
     // Start is called before the first frame update
     void Start()
@@ -13,8 +14,9 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = player.transform.position;
+        //offset the camera behind the player by adding to the player's position
+        transform.position = player.transform.position + offset;
     }
 }
--- Assets/Scripts/PlayerContraller.cs
+++ Assets/Scripts/PlayerContraller.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerContraller : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizantalInput;
+    public float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,12 @@ public class PlayerContraller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // This is where we got players input
+        horizantalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+ 
+       
+         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizantalInput);
     }
 }
